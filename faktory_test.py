2afd9c94bb9c090af6032f8fc91365ebe3de8048
Yashwant <65294199+yashzord@users.noreply.github.com>
2024-10-11 14:17:56 -0400

    Update faktory_test.py

--- faktory_test.py
+++ faktory_test.py
@@ -1,18 +1,47 @@
-import faktory
-import pymongo
-import json
+import logging
+from pyfaktory import Client, Consumer, Job, Producer
+import time
+import random
 
-def store_post(post_data, board):
-    post = json.loads(post_data)
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["fourchan"]
-    collection = db[board]
-    collection.insert_one(post)
+logger = logging.getLogger("faktory test")
+logger.propagate = False
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+sh.setFormatter(formatter)
+logger.addHandler(sh)
 
-if __name__ == '__main__':
-    with faktory.connection() as client:
-        while True:
-            with client.fetch(queue='default') as job:
-                if job:
-                    store_post(*job['args'])
-                    job.ack()
+
+def adder(x, y):
+    logger.info(f"{x} + {y} = {x + y}")
+    # make random sleep time?
+    sleep_for = random.randint(0, 10)
+    time.sleep(sleep_for)
+    logger.info(f"finished sleeping")
+
+
+if __name__ == "__main__":
+    # Default url for a Faktory server running locally
+    faktory_server_url = "tcp://:password@localhost:7419"
+
+    with Client(faktory_url=faktory_server_url, role="producer") as client:
+        producer = Producer(client=client)
+
+        # generate 10 adder jobs
+        jobs = []
+        for job_num in range(10):
+            # see https://github.com/ghilesmeddour/faktory_worker_python/blob/main/src/pyfaktory/models.py
+            # what a `Job` looks like
+            job = Job(jobtype="adder", args=(job_num, 4), queue="default")
+            # job = Job(jobtype="adder", args=(job_num, 'a'), queue="default")
+            # producer.push(job)
+            jobs.append(job)
+
+        producer.push_bulk(jobs)
+
+    print("Starting consumer")
+
+    with Client(faktory_url=faktory_server_url, role="consumer") as client:
+        consumer = Consumer(client=client, queues=["default"], concurrency=3)
+        consumer.register("adder", adder)
+        consumer.run()
